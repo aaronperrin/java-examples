5b713b1ffa0d50651c00fc05e638d911eaa8b59e
Aaron <aperrin@gravyanalytics.com>
2017-09-12 16:56:04 -0700

    added more tests

--- src/main/java/com/afpx/exercises/CoinMachine.java
+++ src/main/java/com/afpx/exercises/CoinMachine.java
@@ -4,115 +4,7 @@ import java.util.*;
 
 public class CoinMachine {
 
-    /*
-        Need findFactors tree data structure
-        So that {3, 6, 7. 9} can be represented as
-        3 -> {3}
-        6 -> {6}, {3, 3}
-        7 -> {7}
-        9 -> {9}, {6, 3}
 
-        target0 = 20
-        current0 = 9
-            target1 = target0 - current0 = 11
-            current1 = 9
-                target2 = target1 - current1 = 2
-                current2 = NONE
-                <----
-            current1 = 7
-                target2 = target1 - current1 = 4
-                current2 = 3
-                    target3 = target2 - current2 = 1
-                    current3 = NONE
-                    <---
-                current2 = NONE
-                <---
-            current1 = 6
-                target2 = target1 - current1 = 5
-                current2 = 3
-                    target3 = target2 - current2 = 2
-                    current3 = NONE
-                    <---
-                current2 = NONE
-                <---
-            current1 = 3
-                target2 = target1 - current1 = 8
-                current2 = 7
-                    target3 = target2 - current2 = 1
-                    current3 = NONE
-                    <---
-                current2 = 6
-                    target3 = target2 - current2 = 2
-                    current3 = NONE
-                    <---
-                current2 = 3
-                    target3 = target2 - current2 = 5
-                    current3 = 3
-                        target4 = target3 - current3 = 2
-                        current4 = NONE
-                        <---
-                    current3 = NONE
-                    <---
-                current2 = NONE
-                <---
-           current1 = NONE
-           <---
-        current0 = 7
-            target1 = target0 - current0 = 13
-            current1 = 6
-                target2 = target1 - current1 = 1
-                current2 = NONE
-                <--- {}
-            current1 = 3
-                target2 = target1 - current1 = 10
-                current2 = 9
-                    target3 = target2 - current2 = 1
-                    current3 = NONE
-                    <--- {}
-                current2 = 7
-                    target3 = target2 - current2 = 3
-                    current3 = 3
-                        target4 = target3 - current3 = 0
-                    <--- {3}
-                <--- {7, 3}
-            <--- {3, 7, 3}
-        <--- {7, 3, 7, 3}
-
-        target0 = 25
-        current0 = 9
-            target1 = target0 - current0 = 16
-            current1 = 9
-                target2 = target1 - current1 = 7
-                current2 = 7
-                    target3 = target2 - current2 = 0
-                <--- {7}
-            <--- {7, 9}
-        <--- {7, 9, 9}
-
-        target0 = 9
-        current0 = 9
-            target1 = target0 - current0 = 0
-        <--- {9}
-
-        target0 = 8
-        current0 = 7
-            target1 = target0 - current0 = 1
-            current1 = NONE
-            <--- {)}
-        current0 = 6
-            target1 = target0 - current0 = 2
-            current1 = NONE
-            <--- {}
-        current0 = 3
-            target1 = target0 - current0 = 5
-            current1 = 3
-                target2 = target1 - current1 = 2
-                current2 = NONE
-                <--- {}
-            <--- {}
-        <--- {}
-
-         */
 
     private NavigableSet<Integer> coins = new TreeSet<>();
 
@@ -135,8 +27,7 @@ public class CoinMachine {
 
     private boolean findFactors(ArrayList<Integer> outFactors, int targetSum, NavigableSet<Integer> possibleFactors) {
         // Limit the set of factors to only those that are less than the targetSum
-        NavigableSet<Integer> subFactors = possibleFactors.headSet(targetSum, false);
-
+        NavigableSet<Integer> subFactors = possibleFactors.headSet(targetSum, true);
         Iterator<Integer> iterator = subFactors.descendingIterator();
         while(iterator.hasNext()) {
             int factor = iterator.next();
--- src/test/java/com/afpx/exercises/CoinMachineTest.java
+++ src/test/java/com/afpx/exercises/CoinMachineTest.java
@@ -8,17 +8,170 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class CoinMachineTest {
     @Test
-    public void oneToOne() throws Exception {
+    public void simpleSingleFactor() throws Exception {
         CoinMachine cm = new CoinMachine();
         cm.addCoin(1);
         ArrayList<Integer> largestFactors = cm.findLargestFactors(1);
         assertEquals(1, largestFactors.size());
         assertThat(largestFactors.get(0), is(1));
+    }
+
+    /*
+        coins = {3, 6, 7. 9}
+        target0 = 20
+        current0 = 9
+            target1 = target0 - current0 = 11
+            current1 = 9
+                target2 = target1 - current1 = 2
+                current2 = NONE
+                <---- {}
+            current1 = 7
+                target2 = target1 - current1 = 4
+                current2 = 3
+                    target3 = target2 - current2 = 1
+                    current3 = NONE
+                    <--- {}
+                current2 = NONE
+                <--- {}
+            current1 = 6
+                target2 = target1 - current1 = 5
+                current2 = 3
+                    target3 = target2 - current2 = 2
+                    current3 = NONE
+                    <--- {}
+                current2 = NONE
+                <--- {}
+            current1 = 3
+                target2 = target1 - current1 = 8
+                current2 = 7
+                    target3 = target2 - current2 = 1
+                    current3 = NONE
+                    <--- {}
+                current2 = 6
+                    target3 = target2 - current2 = 2
+                    current3 = NONE
+                    <--- {}
+                current2 = 3
+                    target3 = target2 - current2 = 5
+                    current3 = 3
+                        target4 = target3 - current3 = 2
+                        current4 = NONE
+                        <--- {}
+                    current3 = NONE
+                    <--- {}
+                current2 = NONE
+                <--- {}
+           current1 = NONE
+           <--- {}
+        current0 = 7
+            target1 = target0 - current0 = 13
+            current1 = 9
+                target2 = target1 - current1 = 4
+                current2 = 3
+                    target3 = target2 - current2 = 1
+                    current3 = NONE
+                    <--- {}
+                current2 = NONE
+                <-- {}
+            current1 = 7
+                target2 = target1 - current1 = 6
+                current2 = 6
+                    target3 = target2 - current2 = 0
+                <-- {6}
+            <-- {7, 6}
+        <-- {7, 7, 6}
+         */
+    @Test
+    public void testExample1() throws Exception {
+        CoinMachine cm = new CoinMachine();
+        cm.addCoin(3);
+        cm.addCoin(6);
+        cm.addCoin(7);
+        cm.addCoin(9);
+        ArrayList<Integer> largestFactors = cm.findLargestFactors(20);
+        String result = largestFactors.stream().map(Object::toString).collect(Collectors.joining(","));
+        assertThat(result, is("6,7,7"));
+    }
+
+    /*
+        target0 = 25
+        current0 = 9
+            target1 = target0 - current0 = 16
+            current1 = 9
+                target2 = target1 - current1 = 7
+                current2 = 7
+                    target3 = target2 - current2 = 0
+                <--- {7}
+            <--- {7, 9}
+        <--- {7, 9, 9}
+     */
+    @Test
+    public void testExample2() throws Exception {
+        CoinMachine cm = new CoinMachine();
+        cm.addCoin(3);
+        cm.addCoin(6);
+        cm.addCoin(7);
+        cm.addCoin(9);
+        ArrayList<Integer> largestFactors = cm.findLargestFactors(25);
+        String result = largestFactors.stream().map(Object::toString).collect(Collectors.joining(","));
+        assertThat(result, is("7,9,9"));
+    }
+
+    /*
+        coins = {3, 6, 7. 9}
+        target0 = 9
+        current0 = 9
+            target1 = target0 - current0 = 0
+        <--- {9}
+     */
+    @Test
+    public void testExample3() throws Exception {
+        CoinMachine cm = new CoinMachine();
+        cm.addCoin(3);
+        cm.addCoin(6);
+        cm.addCoin(7);
+        cm.addCoin(9);
+        ArrayList<Integer> largestFactors = cm.findLargestFactors(9);
+        String result = largestFactors.stream().map(Object::toString).collect(Collectors.joining(","));
+        assertThat(result, is("9"));
+    }
+
+    /*
+        coins = {3, 6, 7. 9}
+        target0 = 8
+        current0 = 7
+            target1 = target0 - current0 = 1
+            current1 = NONE
+            <--- {)}
+        current0 = 6
+            target1 = target0 - current0 = 2
+            current1 = NONE
+            <--- {}
+        current0 = 3
+            target1 = target0 - current0 = 5
+            current1 = 3
+                target2 = target1 - current1 = 2
+                current2 = NONE
+                <--- {}
+            <--- {}
+        <--- {}
+     */
+    @Test
+    public void testExample4() throws Exception {
+        CoinMachine cm = new CoinMachine();
+        cm.addCoin(3);
+        cm.addCoin(6);
+        cm.addCoin(7);
+        cm.addCoin(9);
+        ArrayList<Integer> largestFactors = cm.findLargestFactors(8);
+        String result = largestFactors.stream().map(Object::toString).collect(Collectors.joining(","));
+        assertThat(result, is(""));
     }
 }
