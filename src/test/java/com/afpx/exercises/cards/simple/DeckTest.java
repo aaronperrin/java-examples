adc7ce7ceb9668bc1ffa583e78e9f80c9896fde1
Aaron <aperrin@gravyanalytics.com>
2018-01-11 15:26:18 -0500

    added a couple unit tests and revised

--- src/test/java/com/afpx/exercises/cards/simple/DeckTest.java
+++ src/test/java/com/afpx/exercises/cards/simple/DeckTest.java
@@ -9,48 +9,97 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 public class DeckTest {
-    @Test
-    public void newDeckHasTopCard() throws Exception {
-        Deck d = new Deck();
-        Optional<Card> topCard = d.dealOneCard();
-        assertTrue("Top card of deck should exist", topCard.isPresent());
-    }
 
+    /**
+     * A new deck should have 52 cards
+     */
     @Test
-    public void noCardIsDealtAfter52() throws Exception {
+    public void newDeckHas52Cards() throws Exception {
         Deck d = new Deck();
         for(int i = 0; i < 52; i++) {
             assertTrue("A deck should have 52 cards", d.dealOneCard().isPresent());
         }
+    }
+
+    /**
+     * A shuffled deck should have 52 cards
+     */
+    @Test
+    public void shuffledDeckHas52Cards() throws Exception {
+        Deck d = new Deck();
+        d.shuffle();
+        for(int i = 0; i < 52; i++) {
+            assertTrue("A deck should have 52 cards", d.dealOneCard().isPresent());
+        }
+    }
+
+    /**
+     * After a deck is consumed, all further deals should not return a card
+     */
+    @Test
+    public void noCardIsDealtAfter52() throws Exception {
+        Deck d = new Deck();
+        for(int i = 0; i < 52; i++) {
+            d.dealOneCard();
+        }
         assertFalse("No card should be returned when all cards have been dealt", d.dealOneCard().isPresent());
     }
 
+    /**
+     * After deck is used and then reshuffled, 52 cards are in shuffled deck
+     */
+    @Test
+    public void deckHas52CardsAfterReshuffle() throws Exception {
+        // Start with a new deck, shuffle it, and deal all cards.
+        Deck d = new Deck();
+        for(int i = 0; i < 52; i++) {
+            d.dealOneCard();
+        }
+        // Reshuffle, and check for 52 cards
+        d.shuffle();
+        for(int i = 0; i < 52; i++) {
+            assertTrue("A reshuffled deck should have 52 cards", d.dealOneCard().isPresent());
+        }
+    }
+
+    /**
+     * Cards of all rank and suit exist in the deck
+     */
     @Test
     public void newDeckHasAllCards() throws Exception {
         Deck d = new Deck();
+        // Go through each card in the deck. None of the cards should repeat
         Set<Card> testSet = new HashSet<>();
-        Card topCard = d.dealOneCard().orElse(null);
+        Card card = d.dealOneCard().orElse(null);
         int cardCount = 0;
-        while(topCard != null) {
+        while(card != null) {
             cardCount++;
-            assertTrue("Top card should not have been encountered yet", testSet.add(topCard));
-            topCard = d.dealOneCard().orElse(null);
+            assertTrue("There should not be duplicate cards in the deck", testSet.add(card));
+            card = d.dealOneCard().orElse(null);
         }
-        assertEquals("52 cards should have been present", 52, cardCount);
+        assertEquals("52 cards should be present", 52, cardCount);
     }
 
+    /**
+     * A new deck is always initialized in the same state. Therefore, two new decks should have same order.
+     */
     @Test
     public void twoNewDecksHaveSameCardOrder() throws Exception {
         Deck d1 = new Deck();
         Deck d2 = new Deck();
         for (int i = 0; i < 52; i++) {
-            assertTrue("Two new decks should have same cards and order",
-                    d1.dealOneCard().get().equals(d2.dealOneCard().get()));
+            assertTrue(
+                    "Two new decks should have same cards and order",
+                    d1.dealOneCard().get().equals(d2.dealOneCard().get())
+            );
         }
     }
 
+    /**
+     * A shuffled deck should have a different order
+     */
     @Test
-    public void shuffledDeckHasDifferentCardOrder() throws Exception {
+    public void shuffledDeckVariesFromUnshuffled() throws Exception {
         Deck initial = new Deck();
         Deck shuffled = new Deck();
         shuffled.shuffle();
@@ -59,10 +108,33 @@ public class DeckTest {
         for (int i = 0; i < 52; i++) {
             isSameOrder = shuffled.dealOneCard().get().equals(initial.dealOneCard().get());
             if(!isSameOrder) {
+                // break if the decks are verified to be different
                 break;
             }
         }
 
         assertFalse("A shuffled deck should have a different order than an un-shuffled deck", isSameOrder);
+    }
+
+    /**
+     * Two different shuffled decks should have different order
+     */
+    @Test
+    public void twoShuffledDecksAreDifferent() throws Exception {
+        Deck d1 = new Deck();
+        Deck d2 = new Deck();
+        d1.shuffle();
+        d2.shuffle();
+
+        boolean isSameOrder = true;
+        for (int i = 0; i < 52; i++) {
+            isSameOrder = d1.dealOneCard().get().equals(d2.dealOneCard().get());
+            if(!isSameOrder) {
+                // break if the decks are verified to be different
+                break;
+            }
+        }
+
+        assertFalse("Two shuffled decks should have different order", isSameOrder);
     }
 }
